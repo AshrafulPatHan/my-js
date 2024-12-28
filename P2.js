fe9f03afc5f9e58424e4d89ca54a91f6c1777293
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2024-12-28 08:24:42 +0600

    first commit

---
+++ P2.js
@@ -0,0 +1,8 @@
+var a = 10 ;
+var b = 20 ;
+var c = "Hello" ;
+var d = "Mama" ;
+var e = " " ;
+console.log(a+b);
+console.log(c+d);
+console.log(c+e+d);
---
+++ P3.js
@@ -0,0 +1,15 @@
+// True/False
+var a = true ;
+var b = false ;
+var c = "Mama" ;
+var d = "Tata" ;
+
+
+// IF/Else
+if (b === true) {
+    console.log(c);
+}
+else{
+    console.log(d);
+    
+}
