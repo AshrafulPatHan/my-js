bc9564ad1b265d220f133ebe8799f519e3d579ac
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-01-28 09:51:28 +0600

    add arrey

---
+++ Arrays18.js
@@ -0,0 +1,8 @@
+// node Arrays18.js
+let Array = ['Appel','Banana','Orange'];
+
+let fruits1 = Array[0];
+console.log(fruits1);
+
+
+
--- JavaScriptSpecials15.js
+++ JavaScriptSpecials15.js
@@ -1,4 +1,10 @@
+    //  node JavaScriptSpecials15.js
     console.log("hello world");
     let user = {na : 2,ho : 4}; // "object constructor" syntax
     console.log(user);
-    
+    // Experiment
+
+let exp = console.log(user);
+
+console.log(exp);
+
